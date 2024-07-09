da6e1a4adfbe18154e7d3ba6cc2f934a23fed644
rizkirobawa <rizkirobawa@gmail.com>
2024-07-09 23:21:19 +0700

    fix: remove unused code and adjustment seeder

--- routes/v1/airline.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const airline = require('../../controllers/airline.controllers');
-const upload = require('../../middlewares/upload.middleware');
-const router = Router();
-const { restrict } = require('../../middlewares/auth.middleware');
-
-router.put('/:id/logo', restrict, upload.single('file'), airline.updateLogo);
-
-module.exports = router;
--- routes/v1/index.js
+++ routes/v1/index.js
@@ -7,7 +7,6 @@ const authRoutes = require('./auth.routes');
 const bookingRoutes = require('./booking.routes');
 const flightRoutes = require('./flight.routes');
 const paymentRoutes = require('./payment.routes')
-const airlineRoutes = require('./airline.routes');
 const notificationRoutes = require('./notification.routes')
 const ticketRoutes = require('./ticket.routes');
 const cityRoutes = require('./city.routes');
@@ -33,7 +32,6 @@ router.use('/api/v1/auth', authRoutes);
 router.use('/api/v1/bookings', bookingRoutes);
 router.use('/api/v1/flights', flightRoutes);
 router.use('/api/v1/payments', paymentRoutes);
-router.use('/api/v1/airlines', airlineRoutes);
 router.use('/api/v1/notifications', notificationRoutes);
 router.use('/api/v1/tickets', ticketRoutes);
 router.use('/api/v1/cities', cityRoutes);
