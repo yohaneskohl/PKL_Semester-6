852d30762a8c35bfe3de1ef67862b870fafa79b4
rizkirobawa <rizkirobawa@gmail.com>
2024-07-09 23:11:24 +0700

    docs: added documentation for all API was created

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -10,6 +10,23 @@ const paymentRoutes = require('./payment.routes')
 const airlineRoutes = require('./airline.routes');
 const notificationRoutes = require('./notification.routes')
 
+const swaggerUI = require('swagger-ui-express');
+const yaml = require('yaml');
+const fs = require('fs');
+const path = require('path');
+
+const swagger_path = path.resolve(__dirname, '../../docs/v1.yaml');
+const customCssUrl =
+  'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css';
+const customJs = [
+  'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+];
+
+const file = fs.readFileSync(swagger_path, 'utf-8');
+
+const swaggerDocument = yaml.parse(file);
+
 router.use('/api/v1/auth', authRoutes);
 router.use('/api/v1/bookings', bookingRoutes);
 router.use('/api/v1/flights', flightRoutes);
